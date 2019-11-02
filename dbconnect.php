f7a6e0510e7e738311ea042e4190ce5d0706a7e9
KCOMKCOM <51163016+KCOMKCOM@users.noreply.github.com>
2019-11-02 23:46:33 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host= "61.7.241.82" ;
-$userr="root";
-$pwd= "KCOM789456" ;
-$dbname="line_notify";
+$host = "61.7.241.82" ;
+$userr = "root";
+$pwd = "KCOM789456" ;
+$dbname = "line_notify";
 $link = mysqli_connect($host, $userr, $pwd, $dbname);
 mysqli_query($link, "SET NAMES utf8");
 mysqli_query($link, "SET character_set_results=utf8");
