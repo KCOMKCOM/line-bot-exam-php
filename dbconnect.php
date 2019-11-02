3b949da073f59693d21ebacf459e2242a5a90f5b
KCOMKCOM <51163016+KCOMKCOM@users.noreply.github.com>
2019-11-03 00:41:41 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,5 +4,6 @@ $Setup_User = 'root';
 $Setup_Pwd = 'm@dical' ;
 $Setup_Database = 'line_notify';
 mysql_connect($Setup_Server,$Setup_User,$Setup_Pwd);
+mysql_query("use $Setup_Database");
 mysql_query("SET NAMES UTF8");
 ?>
