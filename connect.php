74938a8823912b5ae90ca57ea98687dfa1b2dad1
KCOMKCOM <51163016+KCOMKCOM@users.noreply.github.com>
2019-11-04 20:41:12 +0700

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$host= "61.7.241.82" ;
+$userr="root";
+$pwd= "KCOM789456" ;
+$dbname="line_notify";
+$link = mysqli_connect($host, $userr, $pwd, $dbname);
+mysqli_query($link, "SET NAMES utf8");
+mysqli_query($link, "SET character_set_results=utf8");
+mysqli_query($link, "SET character_set_client=utf8");
+mysqli_query($link, "SET character_set_connection=utf8");
+?>
